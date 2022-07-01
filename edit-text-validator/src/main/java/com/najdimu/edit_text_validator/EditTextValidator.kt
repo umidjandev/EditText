3be2fab5269aabd924770a0073e59c9c9ac06e14
Umidjan <97349110+umidjandev@users.noreply.github.com>
2022-07-01 17:23:18 +0500

    first iniiiii

--- edit-text-validator/src/main/java/com/najdimu/edit_text_validator/EditTextValidator.kt
+++ edit-text-validator/src/main/java/com/najdimu/edit_text_validator/EditTextValidator.kt
@@ -2,7 +2,7 @@ package com.najdimu.edit_text_validator
 
 import android.widget.EditText
 
-class EditTextValidator {
+ object EditTextValidator {
 
     fun showError(editText: EditText, message: String) {
         editText.setError(message)
